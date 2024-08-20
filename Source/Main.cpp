43e1455d0489d21842d28d874ad2c841a4445cd2
JaeHak <woofer882@gmail.com>
2024-08-20 23:32:45 +0900

    비주얼 스튜디오 프로젝트 생성으로 인한 폴더구조 개편

---
+++ Source/Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+/* 함수 선언 부분*/
+void MainScreen();
+
+
+
+
+int main()
+{
+	cout << "지우기 1초 전" << endl;
+	Sleep(1000);
+	system("cls");
+	Sleep(1000);
+	cout << "지우기 완료";
+
+}
