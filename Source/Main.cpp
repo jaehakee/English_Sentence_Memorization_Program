4c40dda9a3875ab3fcc6a2a748a68c396bbee14a
JaeHak <woofer882@gmail.com>
2024-08-28 23:01:10 +0900

    feat: create screen file and header #3

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -3,18 +3,8 @@
 
 using namespace std;
 
-/* 함수 선언 부분*/
-void MainScreen();
-
-
-
-
 int main()
 {
-	cout << "지우기 1초 전" << endl;
-	Sleep(1000);
-	system("cls");
-	Sleep(1000);
-	cout << "지우기 완료";
+
 
 }
